e7522ce599db8bcc968d86e331f751c0249d9bf7
GigaN1ga <loppupossum@gmail.com>
2023-03-12 23:35:54 +0600

    hw2

--- hero.py
+++ hero.py
@@ -19,15 +19,59 @@ class SuperHero:
     def __str__(self):
         return f'{self.nick()}\n' \
                f'my superpower is {self.superpower}\n' \
-               f'{self.hp_x2()}'
+               f'{self.health_points}'
 
 
     def __len__(self):
         return len(f'{self.catchphrase}')
 
-hero = SuperHero('Amir', 'Allmight', 'fire', 100 , 'i love tacoo')
-print(hero)
-print(len(hero))
-print(hero.heronamme())
-print(hero.hp_x2())
-print(hero.nick())
+class Earth_hero(SuperHero):
+    people = 'people'
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, fly=False, damage=False):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.fly = fly
+        self.damage = damage
+    def hp_x2(self):
+        self.fly = True
+        return f'health: {self.health_points ** 2}'
+
+    def fly_sky(self):
+        print(f'fly in the {self.fly}_phrase')
+
+
+class Cosmic_hero(SuperHero):
+    people = 'people'
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, fly=False, damage=False):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.fly = fly
+        self.damage = damage
+    def hp_x2(self):
+        self.fly = True
+        return f'health: {self.health_points ** 2}'
+
+    def fly_sky(self):
+        print(f'fly in the {self.fly}_phrase')
+
+
+class Villian(Cosmic_hero):
+    SuperHero.people = 'monster'
+    def __init__(self, name, nickname, superpower, health_points, catchphrase):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+
+    def gen_x(self): ...
+
+    def crit(self, hero):
+        return hero.damage ** 2
+
+    def __str__(self):
+        return f'{self.damage}'
+
+thor = Cosmic_hero('thor', 'God', 'Thunder', 200, 'Im thor odinson!', damage=300)
+Toni = Earth_hero('cap', 'pon', 'Mind', 150, 'Jarviz', damage=100)
+thor.hp_x2()
+thor.fly_sky()
+Toni.hp_x2()
+Toni.fly_sky()
+tanos = Villian('tanos', 'Conquerer', 'Gauntlet', 900, 'Try me bitch')
+print(Villian.crit(tanos, thor))
+print(Villian.crit(tanos, Toni))
